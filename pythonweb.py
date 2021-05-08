cf781b99ab3f1492a3e44936c1d413c62170e6a2
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-05-08 14:19:49 +0530

    Update : template Inheritance
    Any HTML page can inherit from a base HTML
    Hyperlinkeing with jinja syntax {{ url_for( route_Function )}}

--- pythonweb.py
+++ pythonweb.py
@@ -1,17 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "<h1>My Name is Manish JHA</h1>"
 
+
 @app.route('/about/<username>')
 def about_page(username):
     return f"<h4>The About page tells about {username}</h4>"
 
+
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
 
 @app.route('/market')
 def market_page():
@@ -20,4 +25,4 @@ def market_page():
         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
-    return render_template('market.html',item_name=items)
+    return render_template('market.html', items=items)
