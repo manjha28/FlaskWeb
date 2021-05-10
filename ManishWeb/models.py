51e51672d06d9775b222874f218429ad9abcb261
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-05-10 20:56:01 +0530

    Update : Relationship with Database
    Couldn't get the ForeignKey relate with Primary Key.
    Will Look into it LAter

--- ManishWeb/models.py
+++ ManishWeb/models.py
@@ -1,14 +1,22 @@
 from ManishWeb import db
 
+'''db.drop_all(),db.create_all(),item1 = Item.query.filter_by(name = 'Iphone 10'),i = Item.query.filter_by(name='Iphone').first().id,
+'''
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    password = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
 
 class Item(db.Model):
-    #Don't know the reason why the reference cannot be find
-    id = db.Column(db.Integer(),primary_key = True)
-    name = db.Column(db.String(length = 30),nullable = False,unique = True)
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=14), nullable=False, unique=True)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
-
-    #A magic method which returns the variable we want instead of default options
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
