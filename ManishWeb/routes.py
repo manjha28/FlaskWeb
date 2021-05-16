40cb2140476781019b77e86feca2d9cd900609f4
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-05-16 23:22:34 +0530

    Update : Validations in the flask form
    Addded a script to add the data collected to the db
    Basic email and password confirmation validations are added

--- ManishWeb/routes.py
+++ ManishWeb/routes.py
@@ -1,7 +1,9 @@
 from ManishWeb import app
-from flask import render_template
-from ManishWeb.models import Item
+from flask import render_template, redirect, url_for
+from ManishWeb.models import Item,User
 from ManishWeb.forms import RegisterForm
+from ManishWeb import db
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -27,7 +29,18 @@ def market_page():
     return render_template('market.html', items=items)
 
 '''Rote for register page which contains a form'''
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username= form.username.data,
+                              email = form.email.data,
+                              password = form.password1.data)
+
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: # If there's no error message in the dictionary
+        for err_msg in form.errors.values():
+            print(f"The error is {err_msg}")
     return render_template('register.html',form = form)
