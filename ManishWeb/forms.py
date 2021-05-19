3cdf96a6c404d74fad7e246725db029e0fa1cd6b
Manish Jha <58687464+manjha28@users.noreply.github.com>
2021-05-19 19:27:42 +0530

    Update : Complex Validations
    SQL Alchemy error handling by creating different validation functions.

--- ManishWeb/forms.py
+++ ManishWeb/forms.py
@@ -1,8 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from ManishWeb.models import User
+
 
 class RegisterForm(FlaskForm):
+    '''To get a validation FlaskForm class searches for function starting with validate followed by underscore and the parameter we want to validate'''
+    def validate_username(self,username_to_check):
+        user = User.query.filter_by(username = username_to_check.data).first() #returns an object of User
+        if user:
+            raise ValidationError("Username already Exists! Try different username")
+
+    def validate_email(self,email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()  # returns an object of User
+        if email:
+            raise ValidationError("Email already Exists! Try different one")
+
+
     username = StringField(label='User Name:',validators=[Length(min=2,max=30), DataRequired()])
     email = StringField(label='Email:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label ='Password:',validators=[Length(min=6), DataRequired()])
